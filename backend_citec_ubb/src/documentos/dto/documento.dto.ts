bb313f412bfbed401b084de6403cb6dfba073d6b
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2025-03-10 16:32:10 -0300

    Fix formatting issues in CrearDocumentoDto and ActualizarDocumentoDto for consistency

--- backend_citec_ubb/src/documentos/dto/documento.dto.ts
+++ backend_citec_ubb/src/documentos/dto/documento.dto.ts
@@ -24,7 +24,7 @@ export class CrearDocumentoDto {
     @IsNotEmpty({ message: 'El nombre del proyecto esta vacio' })
     @Transform(({ value }) => {
         if (typeof value !== 'string') return value;
-        return value.toUpperCase()
+        return value.toUpperCase();
     })
     readonly nombre: string;
 
@@ -37,7 +37,7 @@ export class CrearDocumentoDto {
     @IsNotEmpty({ message: 'El nombre del ejecutor esta vacio' })
     @Transform(({ value }) => {
         if (typeof value !== 'string') return value;
-        return value.toUpperCase()
+        return value.toUpperCase();
     })
     readonly ejecutor: string;
 
@@ -53,7 +53,7 @@ export class CrearDocumentoDto {
     @IsNotEmpty({ message: 'El nombre del cliente esta vacio' })
     @Transform(({ value }) => {
         if (typeof value !== 'string') return value;
-        return value.toUpperCase()
+        return value.toUpperCase();
     })
     readonly cliente: string;
 
@@ -66,12 +66,12 @@ export class CrearDocumentoDto {
     @IsNotEmpty({ message: 'El nombre la direccion esta vacio' })
     @Transform(({ value }) => {
         if (typeof value !== 'string') return value;
-        return value.toUpperCase()
+        return value.toUpperCase();
     })
-    readonly direccion: string
+    readonly direccion: string;
 
     @IsIn(Object.values(AREAS_DE_DOCUMENTO), {
-        message: 'El area debe ser uno de los valores permitidos'
+        message: 'El area debe ser uno de los valores permitidos',
     })
     @Length(1, 50, {
         message:
@@ -93,21 +93,24 @@ export class CrearDocumentoDto {
     @Matches(/^\d{4}-\d{2}-\d{2}$/, {
         message: 'La fecha de solicitud debe tener el formato YYYY-MM-DD',
     })
-    @ApiProperty({ description: 'Esta es la fecha de inicio del proyecto', })
-    readonly fecha_inicio: Date
+    @ApiProperty({ description: 'Esta es la fecha de inicio del proyecto' })
+    readonly fecha_inicio: Date;
 
     @IsNotEmpty({ message: 'La fecha de finalizacion esta vacia' })
     @Matches(/^\d{4}-\d{2}-\d{2}$/, {
         message: 'La fecha de solicitud debe tener el formato YYYY-MM-DD',
     })
-    @ApiProperty({ description: 'Esta es la fecha de finalizacion del proyecto' })
-    readonly fecha_finalizacion: Date
+    @ApiProperty({
+        description: 'Esta es la fecha de finalizacion del proyecto',
+    })
+    readonly fecha_finalizacion: Date;
 
     @IsIn(Object.values(VALIDEZ_DE_DOCUMENTO), {
-        message: 'La validez debe ser uno de los valores permitidos'
+        message: 'La validez debe ser uno de los valores permitidos',
     })
     @Length(1, 50, {
-        message: 'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
+        message:
+            'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
     })
     @IsString({ message: 'El estado de validez debe ser texto' })
     @IsNotEmpty({ message: 'El estado de validez esta vacio' })
@@ -119,8 +122,7 @@ export class CrearDocumentoDto {
         description: 'Este es el codigo de area',
         default: 'AA',
     })
-    readonly validez_documento: string
-
+    readonly validez_documento: string;
 }
 export class obtenerDocumentoPorIdDto {
     @IsNumber({}, { message: 'Numero del documento' })
@@ -146,33 +148,35 @@ export class ActualizarDocumentoDto extends CrearDocumentoDto {
         default: 'MASA DE UN FOTON',
     })
     @Length(1, 50, {
-        message: 'La longitud del nombre del proyecto debe ser entre 1 y 50 caracteres',
+        message:
+            'La longitud del nombre del proyecto debe ser entre 1 y 50 caracteres',
     })
     @IsString({ message: 'El nombre del proyecto debe ser texto' })
     @IsNotEmpty({ message: 'El nombre del proyecto esta vacio' })
     @Transform(({ value }) => {
         if (typeof value !== 'string') return value;
-        return value.toUpperCase()
+        return value.toUpperCase();
     })
     readonly nuevo_nombre: string;
     @Length(1, 100, {
-        message: 'La longitud la direccion debe ser entre 1 y 100 caracteres'
+        message: 'La longitud la direccion debe ser entre 1 y 100 caracteres',
     })
     @IsString({
-        message: 'El nombre la direccion debe ser un nombre valido'
+        message: 'El nombre la direccion debe ser un nombre valido',
     })
     @IsNotEmpty({ message: 'El nombre la direccion esta vacio' })
     @Transform(({ value }) => {
         if (typeof value !== 'string') return value;
-        return value.toUpperCase()
+        return value.toUpperCase();
     })
-    readonly nueva_direccion: string
+    readonly nueva_direccion: string;
 
     @IsIn(Object.values(AREAS_DE_DOCUMENTO), {
-        message: 'El area debe ser uno de los valores permitidos'
+        message: 'El area debe ser uno de los valores permitidos',
     })
     @Length(1, 50, {
-        message: 'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
+        message:
+            'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
     })
     @IsString({ message: 'El codigo del area debe ser texto' })
     @IsNotEmpty({ message: 'El codigo del area esta vacio' })
@@ -184,27 +188,30 @@ export class ActualizarDocumentoDto extends CrearDocumentoDto {
         description: 'Este es el codigo de area',
         default: 'AA',
     })
-    readonly nueva_area_documento: string
+    readonly nueva_area_documento: string;
 
     @IsNotEmpty({ message: 'La fecha de inicio esta vacia' })
     @Matches(/^\d{4}-\d{2}-\d{2}$/, {
         message: 'La fecha de solicitud debe tener el formato YYYY-MM-DD',
     })
-    @ApiProperty({ description: 'Esta es la fecha de inicio del proyecto', })
-    readonly nueva_fecha_inicio: Date
+    @ApiProperty({ description: 'Esta es la fecha de inicio del proyecto' })
+    readonly nueva_fecha_inicio: Date;
 
     @IsNotEmpty({ message: 'La fecha de finalizacion esta vacia' })
     @Matches(/^\d{4}-\d{2}-\d{2}$/, {
         message: 'La fecha de solicitud debe tener el formato YYYY-MM-DD',
     })
-    @ApiProperty({ description: 'Esta es la fecha de finalizacion del proyecto' })
-    readonly nueva_fecha_finalizacion: Date
+    @ApiProperty({
+        description: 'Esta es la fecha de finalizacion del proyecto',
+    })
+    readonly nueva_fecha_finalizacion: Date;
 
     @IsIn(Object.values(VALIDEZ_DE_DOCUMENTO), {
-        message: 'La validez debe ser uno de los valores permitidos'
+        message: 'La validez debe ser uno de los valores permitidos',
     })
     @Length(1, 50, {
-        message: 'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
+        message:
+            'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
     })
     @IsString({ message: 'El estado de validez debe ser texto' })
     @IsNotEmpty({ message: 'El estado de validez esta vacio' })
@@ -216,14 +223,17 @@ export class ActualizarDocumentoDto extends CrearDocumentoDto {
         description: 'Este es el codigo de area',
         default: 'AA',
     })
-    readonly nueva_validez_documento: string
+    readonly nueva_validez_documento: string;
 }
-export class EliminarDocumentoDto extends PickType(obtenerDocumentoPorIdDto, ['numero']) {
+export class EliminarDocumentoDto extends PickType(obtenerDocumentoPorIdDto, [
+    'numero',
+]) {
     @IsIn(Object.values(VALIDEZ_DE_DOCUMENTO), {
-        message: 'La validez debe ser uno de los valores permitidos'
+        message: 'La validez debe ser uno de los valores permitidos',
     })
     @Length(1, 50, {
-        message: 'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
+        message:
+            'La longitud del nombre del tipo debe ser entre 1 y 50 caracteres',
     })
     @IsString({ message: 'El estado de validez debe ser texto' })
     @IsNotEmpty({ message: 'El estado de validez esta vacio' })
@@ -235,5 +245,5 @@ export class EliminarDocumentoDto extends PickType(obtenerDocumentoPorIdDto, ['n
         description: 'Este es el codigo de area',
         default: 'AA',
     })
-    readonly nueva_validez_documento: string
+    readonly nueva_validez_documento: string;
 }
