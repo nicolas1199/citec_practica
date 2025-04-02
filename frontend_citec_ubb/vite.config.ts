48785462862548a161487b8e9b364b8a6a0a4cfc
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2025-04-02 12:17:38 -0300

    Add server configuration to Vite for development on port 3000

--- frontend_citec_ubb/vite.config.ts
+++ frontend_citec_ubb/vite.config.ts
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@tabler/icons-react':
-        '@tabler/icons-react/dist/esm/icons/index.mjs',
-      'datatables.net': 'datatables.net', // Solo la ruta del paquete
-      'datatables.net-dt': 'datatables.net-dt', // Solo la ruta del paquete
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@tabler/icons-react':
+                '@tabler/icons-react/dist/esm/icons/index.mjs',
+            'datatables.net': 'datatables.net', // Solo la ruta del paquete
+            'datatables.net-dt': 'datatables.net-dt', // Solo la ruta del paquete
+        },
     },
-  },
+    server: {
+        port: 3000,
+    },
 });
